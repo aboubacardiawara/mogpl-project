4c898bfa3d3c2d3cc9db8a727218d6399ce47cf3
diawara <aboubacar.diawara.etu@univ-lille.fr>
2022-11-27 16:25:46 +0100

    template

---
+++ src/exemple.py
@@ -0,0 +1,25 @@
+from gurobipy import *
+
+
+
+variable_count = 0
+constraint_count = 0
+lignes = range(constraint_count)
+colonnes = range(variable_count)
+
+# 1. MATRICE DES CONTRAINTES
+## 1.1 premiers membres 
+
+## 1.2 seconds membres
+
+# 2. COEFFICIENTS DE LA FONCTION OBJECTIF
+
+# 3. DECLARATION DES VARIABLES DE DECISION
+
+# 4. INTEGRATION DES NOUVELLES VARIABLES
+
+# 5. DEFINITION DE L'OBJECTIF
+
+# 6. DEFINITION DES CONTRAINTES
+
+# 7. RESOLUTION
